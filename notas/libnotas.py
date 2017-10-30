fb020cd784915c304c46e9e9dae7c6b7233a32e6
josedom24 <josedom24@gmail.com>
2017-10-30 19:27:05 +0100

    notas

--- notas/libnotas.py
+++ notas/libnotas.py
@@ -1,5 +1,6 @@
 from lxml import html
 import requests
+import copy
 class Notas:
 	def __init__(self):
 		self.url=["https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4-twYojuwv8QEbSJdIDT3JCpOKYVNo3sL-kTGSz_aaIa6835nCJFRkniw12zwBqnh73YSHZppF56G/pubhtml#",
@@ -8,15 +9,19 @@ class Notas:
 		self.info={}
 		for url,tit in zip(self.url,self.titulos):
 			page = requests.get(url)
-			tree = html.fromstring(page.content)
+			utf8_parser = html.HTMLParser(encoding='utf-8')
+			tree = html.fromstring(page.content,parser=utf8_parser)
 			titulos=tree.xpath('//a[@href="#"]/text()')
 			cont=2
 			tables=[]
 			for titulo in titulos[1:]:
 				tables.append([cont,titulo])
 				cont+=1
-			info=[]
+			
+			
+			lista=[]
 			for table in tables:
+				
 				datos=tree.xpath("(//table)[%d]//td"%table[0])
 				datos2=[]
 				for d in datos:
@@ -29,6 +34,8 @@ class Notas:
 			 			else: datos2.append(d.text)
 			 		except:
 			 			datos2.append("")
+			 	
+			 	
 				dic={}
 				dic["titulo"]=table[1]
 				dic["cabeceras"]=[]			
@@ -39,28 +46,28 @@ class Notas:
 				cont=0
 				for i in datos2:
 					cont+=1
-					if  len(i)>10: break
+					if  len(i)>8: break
 					dic["cabeceras"].append(i)
 				datos2=datos2[cont-1:]
 						
 				
-				
 				dic["datos"]={}
 				
 				for i in datos2:
-					if len(i)>10:
+					if len(i)>8:
 						nombre=i
 						dic["datos"][nombre]=[]
 					else:
 						dic["datos"][nombre].append(i)
+				lista.append(dic)
 
-				self.info[tit]=dic
+			self.info[tit]=lista
 				
 
 	def alumnos(self):
 		alumnos={}
 		for tit in self.titulos:
-			alumnos[tit]=self.info[tit]["datos"].keys()
+			alumnos[tit]=self.info[tit][0]["datos"].keys()
 		return alumnos
 
 	def es_alumno(self,nombre):
@@ -71,7 +78,17 @@ class Notas:
 		return nombre in self.alumnos()[modulo]
 
 	def datos(self,nombre):
-		datos={}
+		datos=copy.deepcopy(self.info)
+		print datos is self.info
 		for tit in self.titulos:
-			datos[tit]=self.info[tit]["datos"][nombre]		
+			if not self.es_alumno_modulo(nombre,tit):
+				del datos[tit]
+			else:
+				for practicas in datos[tit]:
+					dat=practicas["datos"]
+					for key,value in dat.items():
+						if key!=nombre:
+							del dat[key]
+
+			
 		return datos
--- notas/views.py
+++ notas/views.py
@@ -6,34 +6,37 @@ from libldap import LibLDAP
 from libnotas import Notas
 
 def index(request):
-    if request.method=="GET":
-            return render(request,'login.html')
-    else:
-        username = request.POST["username"]
-        password = request.POST["password"].encode('utf-8')
-        lldap=LibLDAP(username,password)
-        if lldap.isbind:
-                request.session["username"]=username
-                busqueda='(uid=%s)'%username
-                resultados=lldap.buscar(busqueda)
-                info=resultados[0].get_attributes()
-                return index2(request,username,info["sn"][0]+", "+info["givenname"][0]) 
-        else:
-               info={"error":True}
-               return render(request,"login.html",info)
+	if request.method=="GET":
+			return render(request,'login.html')
+	else:
+		username = request.POST["username"]
+		password = request.POST["password"].encode('utf-8')
+		#lldap=LibLDAP(username,password)
+		#if lldap.isbind:
+		if True:
+				request.session["username"]=username
+				#busqueda='(uid=%s)'%username
+				#resultados=lldap.buscar(busqueda)
+				#info=resultados[0].get_attributes()
+				#return index2(request,username,info["sn"][0]+", "+info["givenname"][0]) 
+				return index2(request,username,"Adan Reina") 
+		else:
+			   info={"error":True}
+			   return render(request,"login.html",info)
 
 
 def index2(request,username,nombre):
-    notas=Notas()
-    print notas.datos("Romero Angulo, María")
-    if notas.es_alumno(nombre):
-        return alumno(request,username,nombre)
-    else:
-        return render(request,'login.html')
+	notas=Notas()
+	if notas.es_alumno(username):
+		return alumno(request,username,nombre)
+	else:
+		return render(request,'login.html')
 	
 def alumno(request,username,nombre):
-    pass
+	notas=Notas()
+	info={"datos":notas.datos(username),"nombre":nombre}
+	return render(request,"index.html",info)    
 
 def salir(request):
-    del request.session["username"]
-    return redirect('/')
+	del request.session["username"]
+	return redirect('/')
