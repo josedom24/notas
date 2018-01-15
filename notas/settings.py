e5e6930986390b9fc25b069d83c1d6c5dc32d853
josedom24 <josedom24@gmail.com>
2018-01-15 19:49:25 +0100

    runserverplus with ssl

--- notas/settings.py
+++ notas/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['notas.gonzalonazareno.org','localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
