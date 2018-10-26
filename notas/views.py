add8ca3833f7797091ee07ddee443324633ea918
José Domingo <josedom24@gmail.com>
2018-10-26 08:12:07 +0200

    notas

--- notas/views.py
+++ notas/views.py
@@ -9,19 +9,20 @@ def index(request):
 	if request.method=="GET":
 			return render(request,'login.html')
 	else:
-#		username = request.POST["username"]
-#		password = request.POST["password"].encode('utf-8')
-#		lldap=LibLDAP(username,password)
-#		if lldap.isbind:
-#				request.session["username"]=username
-#				busqueda='(uid=%s)'%username
-#				resultados=lldap.buscar(busqueda)
-#				info=resultados[0].get_attributes()
-				#return index2(request,username,info["sn"][0]+", "+info["givenname"][0]) 
-				return index2(request,"jesus.arias","Jesús Arias","IAW")
-#		else:
-#			   info={"error":True}
-#			   return render(request,"login.html",info)
+		username = request.POST["username"]
+		password = request.POST["password"].encode('utf-8')
+		modulo = request.POST["modulo"]
+		lldap=LibLDAP(username,password)
+		if lldap.isbind:
+				request.session["username"]=username
+				busqueda='(uid=%s)'%username
+				resultados=lldap.buscar(busqueda)
+				info=resultados[0].get_attributes()
+				return index2(request,username,info["sn"][0]+", "+info["givenname"][0],modulo) 
+				#return index2(request,"jesus.arias","Jesús Arias","IAW")
+		else:
+			   info={"error":True}
+			   return render(request,"login.html",info)
 
 
 def index2(request,username,nombre,modulo):
