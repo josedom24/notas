4b13de011a04dc408f24f9aeefd32b5d1d800846
Cloud-init-user <debian@docker.novalocal>
2018-11-11 18:54:41 +0000

    docker

--- backend/consumer_sheet.py
+++ backend/consumer_sheet.py
@@ -52,5 +52,5 @@ api.add_resource(ConsultaTitulos, '/titulos/<string:modulo>')
 api.add_resource(ConsultaAlumno, '/alumnos/<string:modulo>/<string:nombre>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=0.0.0.0,debug=True)
 
