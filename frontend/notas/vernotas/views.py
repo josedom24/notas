24916e9df32fad9603f49d72d3154712c2614363
Cloud-init-user <debian@docker.novalocal>
2018-11-11 20:27:35 +0000

    docker

--- frontend/notas/vernotas/views.py
+++ frontend/notas/vernotas/views.py
@@ -10,18 +10,18 @@ def index(request):
         password = request.POST["password"].encode('utf-8')
         modulo = request.POST["modulo"]
         request.session["username"]=username
-        #lldap=LibLDAP(username,password)
-        #if lldap.isbind:
-        #		request.session["username"]=username
-        #		busqueda='(uid=%s)'%username
-        #		resultados=lldap.buscar(busqueda)
-        #		info=resultados[0].get_attributes()
-        #		return index2(request,username,info["sn"][0]+", "+info["givenname"][0],modulo) 
-        #
-        #else:
-        #	   info={"error":True}
-        #	   return render(request,"login.html",info)
-        return index2(request,"jesus.arias","Jesús Arias","servicios")
+        lldap=LibLDAP(username,password)
+        if lldap.isbind:
+        		request.session["username"]=username
+        		busqueda='(uid=%s)'%username
+        		resultados=lldap.buscar(busqueda)
+        		info=resultados[0].get_attributes()
+        		return index2(request,username,info["sn"][0].decode()+", "+info["givenname"][0].decode(),modulo) 
+        
+        else:
+        	   info={"error":True}
+        	   return render(request,"login.html",info)
+        #return index2(request,"jesus.arias","Jesús Arias","servicios")
 def index2(request,username,nombre,modulo):
     r = get("http://backend:5000/alumnos/{}/{}".format(modulo,username))
     if r.status_code==200:
@@ -29,6 +29,7 @@ def index2(request,username,nombre,modulo):
         print(r.json())
         return render(request,"index.html",info)    
     else:
+        info={"error":True}
         return render(request,'login.html')
     
     
